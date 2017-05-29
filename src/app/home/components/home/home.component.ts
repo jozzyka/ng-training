943d9fe0ef1d109ee713eaa08fcf72acfec4432e
jozsefszalai <jozsef.szalai@icellmobilsoft.hu>
2017-05-29 15:20:24 +0200

    gkuggkjig

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,25 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-  template: `
-    <div>
-      <h2>{{ 'HOME.TITLE' | translate }}</h2>
-    </div>`
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
   
-  public constructor(private translate:TranslateService) { 
-    translate.addLangs(["en","fr","hu"]);
-    translate.setDefaultLang('en');
+  public constructor() { 
 
-    let browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|fr|hu/)? browserLang : 'en');
   }
 
 
